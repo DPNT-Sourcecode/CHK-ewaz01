6042881939f74ec77c81e076bc7639e6117a71c9
sbx_user1051 <sbx_user1051@169.254.56.141>
2023-06-12 20:53:04 +0000

    Mon Jun 12 20:53:04 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -21,9 +21,17 @@ def checkout(skus):
     sku_freq = defaultdict(int)
 
     for sku in skus:
-        if sku not in price_map.items():
+        if sku not in price_map:
             return -1
         sku_freq[sku] += 1
 
+    total_checkout_value = 0
+    for sku, sku_freq in sku_freq.items():
+        i
+
     
+
+
+
+
 
