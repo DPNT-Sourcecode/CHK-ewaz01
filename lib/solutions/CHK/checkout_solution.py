e01acf067dd4e38270061861c0ebb02b1204bb2e
sbx_user1051 <sbx_user1051@169.254.56.141>
2023-06-12 22:07:17 +0000

    Mon Jun 12 22:07:17 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -19,7 +19,7 @@ def checkout(skus):
     # for each deal/tuple corresponding to a SKU this map takes the form of:
     # tuple[0]SKU for tuple[1]
     offer_map = {
-        "A" : (3, 130),
+        "A" : [(5, 200),(3, 130)],
         "B" : (2, 45)
     }
 
@@ -56,17 +56,24 @@ def checkout(skus):
     total_checkout_value = 0
     for sku, sku_freq in sku_freq.items():
         if sku in offer_map:
-            group_quantity = offer_map[sku][0]
-            group_cost = offer_map[sku][1]
-            groups = sku_freq // group_quantity
-            remaining_individuals = sku_freq - (groups * group_quantity)
-            total = (groups * group_cost) + (remaining_individuals * price_map[sku])
-            total_checkout_value += total
+            deals = offer_map[sku]
+            print(deals)
+            temp_sku_freq = sku_freq
+            for deal in deals:
+                group_quantity = deal[0]
+                group_cost = deal[1]
+                groups = sku_freq // group_quantity
+                temp_sku_freq -= groups * group_quantity
+                total = groups * group_cost
+                total_checkout_value += total
+            total_checkout_value = temp_sku_freq * price_map[sku]
         else:
             total_checkout_value += sku_freq * price_map[sku]
     
     print(total_checkout_value)
     return total_checkout_value
+
+checkout("AAAAAAAA")
 
 def test_checkout_empty():
     assert checkout("") == 0
@@ -85,6 +92,7 @@ def test_checkout_invalid():
 #test_checkout_invalid()
 
     
+
 
 
 
