9859a0703b7906e0181777be6c02b119564b7b7f
sbx_user1051 <sbx_user1051@169.254.56.141>
2023-06-12 20:41:04 +0000

    Mon Jun 12 20:41:04 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,29 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+from collections import defaultdict
+
 def checkout(skus):
-    raise NotImplementedError()
+    price_map = {
+        "A" : 50,
+        "B" : 30,
+        "C" : 20,
+        "D" : 15
+    }
+
+    offer_map = {
+        "A" : (3, 130),
+        "B" : (2, 45)
+    }
+
+    sku_freq = defaultdict(int)
+
+    for sku in skus:
+        if sku not in price_map.items():
+            return -1
+        sku_freq[sku] += 1
+
+    
+
+
