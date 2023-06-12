521470cac7a29bb178d5e479978dfaad24d2694c
sbx_user1051 <sbx_user1051@169.254.56.141>
2023-06-12 20:59:04 +0000

    Mon Jun 12 20:59:04 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -27,9 +27,25 @@ def checkout(skus):
 
     total_checkout_value = 0
     for sku, sku_freq in sku_freq.items():
-        i
+        if sku in offer_map:
+            group_quantity = offer_map[sku][0]
+            group_cost = offer_map[sku][1]
+            groups = sku_freq // group_quantity
+            remaining_individuals = sku_freq - (groups * group_quantity)
+            total = (groups * group_cost) + (remaining_individuals * price_map[sku])
+            total_checkout_value += total
+        else:
+            total_checkout_value += sku_freq * price_map[sku]
+    
+    return total_checkout_value
+
+def test_checkout_empty():
+    assert checkout("") == 0
+
+test_checkout_empty()
 
     
+
 
 
 
